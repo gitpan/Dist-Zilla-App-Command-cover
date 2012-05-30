23b485282d735ec4ae4bd86b9d086aea8617a95f
Mike Doherty <doherty@cpan.org>
2012-05-30 12:46:24 +0000

    Import of DOHERTY/Dist-Zilla-App-Command-cover-1.101001 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-App-Command-cover
    gitpan-cpan-version:      1.101001
    gitpan-cpan-path:         DOHERTY/Dist-Zilla-App-Command-cover-1.101001.tar.gz
    gitpan-cpan-author:       DOHERTY
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/App/Command/cover.pm
+++ lib/Dist/Zilla/App/Command/cover.pm
@@ -1,21 +1,21 @@
+package Dist::Zilla::App::Command::cover;
 use 5.008;
 use strict;
 use warnings;
-
-package Dist::Zilla::App::Command::cover;
-BEGIN {
-  $Dist::Zilla::App::Command::cover::VERSION = '1.101000';
-}
-
 # ABSTRACT: Code coverage metrics for your distribution
+our $VERSION = '1.101001'; # VERSION
+
 use Dist::Zilla::App -command;
-use File::Temp;
-use Path::Class;
-use File::chdir;
+
+
 sub abstract { "code coverage metrics for your distribution" }
 
 sub execute {
     my $self = shift;
+    require File::Temp;
+    require Path::Class;
+    require File::chdir;
+
     local $ENV{HARNESS_PERL_SWITCHES} = '-MDevel::Cover';
     my @cover_command = @ARGV;
 
@@ -34,15 +34,17 @@ sub execute {
     $self->zilla->run_tests_in($target);
 
     $self->log(join ' ' => @cover_command);
-    local $CWD = $target;
+    local $File::chdir::CWD = $target;
     system @cover_command;
     $self->log("leaving $target intact");
 }
-1;
 
+1;
 
 __END__
 =pod
+
+=encoding utf-8
 
 =head1 NAME
 
@@ -50,7 +52,7 @@ Dist::Zilla::App::Command::cover - Code coverage metrics for your distribution
 
 =head1 VERSION
 
-version 1.101000
+version 1.101001
 
 =head1 SYNOPSIS
 
@@ -67,31 +69,41 @@ release tests are not run since they should not be counted against code
 coverage. Any additional command-line arguments are passed to the C<cover>
 command.
 
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
-
-=head1 BUGS AND LIMITATIONS
-
-No bugs have been reported.
-
-Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-App-Command-cover>.
+=for Pod::Coverage abstract execute
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
-site near you, or see
-L<http://search.cpan.org/dist/Dist-Zilla-App-Command-cover/>.
+site near you, or see L<https://metacpan.org/module/Dist::Zilla::App::Command::cover/>.
 
-=head1 AUTHOR
+=head1 SOURCE
 
-  Marcel Gruenauer <marcel@cpan.org>
+The development version is on github at L<http://github.com/doherty/Dist-Zilla-App-Command-cover>
+and may be cloned from L<git://github.com/doherty/Dist-Zilla-App-Command-cover.git>
+
+=head1 BUGS AND LIMITATIONS
+
+You can make new bug reports, and view existing ones, through the
+web interface at L<https://github.com/doherty/Dist-Zilla-App-Command-cover/issues>.
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Marcel Grünauer <marcel@cpan.org>
+
+=item *
+
+Mike Doherty <doherty@cpan.org>
+
+=back
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2010 by Marcel Gruenauer.
+This software is copyright (c) 2010 by Marcel Grünauer <marcel@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
